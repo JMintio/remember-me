b4f223826276724943d26f1b8995b4d330756c56
João Mintio <jvjuaovitor@icloud.com>
2023-10-08 22:22:49 -0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,64 @@
 var btnCover = document.querySelector(".btn-menu-cover");
 var btnMenu = document.querySelector(".btn-menu");
 var sidebar = document.querySelector(".sidebar");
+sidebar.style.display = "none";
 var body = document.querySelector(".notes-box");
 
-btnMenu.addEventListener("mouseover", overBtnHome);
-btnMenu.addEventListener("mouseout", outBtnHome);
 btnMenu.addEventListener("click", openSidebar);
 body.addEventListener("click", closeSidebar);
 
-function overBtnHome() {
-  btnCover.classList.add("over-btn-home");
-  btnCover.classList.remove("out-btn-home");
-  setTimeout(() => {
-    btnCover.style.width = "0px";
-    btnCover.style.height = "0px";
-  }, 200);
-}
-function outBtnHome() {
-  btnCover.classList.remove("over-btn-home");
-  btnCover.classList.add("out-btn-home");
-  setTimeout(() => {
-    btnCover.style.width = "50px";
-    btnCover.style.height = "50px";
-  }, 200);
-}
 function openSidebar() {
-  sidebar.classList.add("openSidebar");
-  setTimeout(() => {
-    sidebar.style.height = "300px";
-    sidebar.classList.remove("openSidebar");
-  }, 200);
+  if (sidebar.style.display == "flex") {
+    closeSidebar();
+  } else {
+    btnMenu.classList.add("anim-btn-menu");
+    sidebar.style.display = "flex";
+    sidebar.classList.add("openSidebar");
+    setTimeout(() => {
+      sidebar.style.height = "100px";
+      sidebar.classList.remove("openSidebar");
+      btnMenu.classList.remove("anim-btn-menu");
+    }, 200);
+  }
 }
 function closeSidebar() {
   sidebar.classList.add("closeSidebar");
   setTimeout(() => {
     sidebar.style.height = "0px";
+    sidebar.style.display = "none";
     sidebar.classList.remove("closeSidebar");
   }, 200);
+}
+
+document.querySelector(".select-notes").addEventListener("click", selectNotes);
+function selectNotes() {
+  var notes = document.querySelectorAll(".note-style-def");
+  notes.forEach((element) => {
+    element.classList.add("shakeAnim");
+  });
+}
+var i = 0;
+document.querySelector(".add-new-note").addEventListener("click", addNewNote);
+function addNewNote() {
+  i++;
+
+  var newNote = document.createElement("div");
+  var noteDesc = document.createElement("input");
+  var noteCheck = document.createElement("div");
+  var noteCheckIcon = document.createElement("img");
+
+  noteDesc.placeholder = "Digite o texto...";
+  newNote.classList.add("note-style-def", "note-style-def", `ID${i}`);
+  noteDesc.classList.add("note-desc-def", `ID_desc${i}`);
+
+  noteCheck.classList.add("note-check", "flex-center", `ID_desc${i}`);
+  noteCheckIcon.classList.add("note-icon-done", `ID_desc${i}`);
+  noteCheckIcon.src = "./img/done.svg";
+
+  var notesBox = document.querySelector(".notes-box");
+  notesBox.appendChild(newNote);
+  newNote.appendChild(noteDesc);
+
+  newNote.appendChild(noteCheck);
+  noteCheck.appendChild(noteCheckIcon);
 }
