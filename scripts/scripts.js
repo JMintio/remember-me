42c1a2f0a6ce4d9a88e9dafa1868593c07b6db06
João Mintio <jvjuaovitor@icloud.com>
2023-10-09 12:43:20 -0300

    UPDATE

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,26 @@
+var body = document.querySelector(".notes-box");
 var btnCover = document.querySelector(".btn-menu-cover");
 var btnMenu = document.querySelector(".btn-menu");
 var sidebar = document.querySelector(".sidebar");
-sidebar.style.display = "none";
-var body = document.querySelector(".notes-box");
+var btnMenuText = document.querySelector(".btn-menu-text");
 
 btnMenu.addEventListener("click", openSidebar);
 body.addEventListener("click", closeSidebar);
 
+sidebar.style.display = "none";
+
 function openSidebar() {
+  // OPEN SIDE BAR FUNCTION
   if (sidebar.style.display == "flex") {
     closeSidebar();
   } else {
-    btnMenu.classList.add("anim-btn-menu");
+    var notes = document.querySelectorAll(".note-style-def");
+    notes.forEach((element) => {
+      element.classList.remove("shakeAnim");
+    });
+    btnMenuText.innerHTML = "menu";
+    btnMenu.classList.add("btn-menu-normal");
+    btnMenu.classList.remove("btn-menu-cancel");
     sidebar.style.display = "flex";
     sidebar.classList.add("openSidebar");
     setTimeout(() => {
@@ -22,6 +31,7 @@ function openSidebar() {
   }
 }
 function closeSidebar() {
+  // CLOSE SIDE BAR FUNCTION
   sidebar.classList.add("closeSidebar");
   setTimeout(() => {
     sidebar.style.height = "0px";
@@ -29,17 +39,25 @@ function closeSidebar() {
     sidebar.classList.remove("closeSidebar");
   }, 200);
 }
-
+/* Select Notes */
 document.querySelector(".select-notes").addEventListener("click", selectNotes);
 function selectNotes() {
+  closeSidebar();
+  btnMenuText.innerHTML = "cancel";
+  btnMenu.classList.remove("btn-menu-normal");
+  btnMenu.classList.add("btn-menu-cancel");
   var notes = document.querySelectorAll(".note-style-def");
   notes.forEach((element) => {
     element.classList.add("shakeAnim");
   });
 }
-var i = 0;
+
+/* Adding notes */
+var i = 1000;
 document.querySelector(".add-new-note").addEventListener("click", addNewNote);
 function addNewNote() {
+  // ADDING NEW NOTES FUNCTION
+  closeSidebar();
   i++;
 
   var newNote = document.createElement("div");
@@ -47,18 +65,19 @@ function addNewNote() {
   var noteCheck = document.createElement("div");
   var noteCheckIcon = document.createElement("img");
 
-  noteDesc.placeholder = "Digite o texto...";
-  newNote.classList.add("note-style-def", "note-style-def", `ID${i}`);
-  noteDesc.classList.add("note-desc-def", `ID_desc${i}`);
-
-  noteCheck.classList.add("note-check", "flex-center", `ID_desc${i}`);
-  noteCheckIcon.classList.add("note-icon-done", `ID_desc${i}`);
+  //NOTE
+  newNote.classList.add("note-style-def", "note-style-def", `ID_01${i}`);
+  //CHECK
+  noteCheck.classList.add("note-check", "flex-center", `ID_02${i}`);
+  noteCheckIcon.classList.add("note-icon-done", `ID_02${i}`);
   noteCheckIcon.src = "./img/done.svg";
+  //DESCRIPTION
+  noteDesc.placeholder = "Digite o texto...";
+  noteDesc.classList.add("note-desc-def", `ID_03${i}`);
 
   var notesBox = document.querySelector(".notes-box");
   notesBox.appendChild(newNote);
-  newNote.appendChild(noteDesc);
-
   newNote.appendChild(noteCheck);
   noteCheck.appendChild(noteCheckIcon);
+  newNote.appendChild(noteDesc);
 }
