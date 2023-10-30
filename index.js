fa42803dc15e0a308869725d91ae1efd1b8847bf
João Mintio <jvjuaovitor@icloud.com>
2023-10-30 20:15:49 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+/* // Import the functions you need from the SDKs you need
+import firebase from "https://cdn.skypack.dev/@firebase/app";
+import "https://cdn.skypack.dev/@firebase/database"; // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -18,4 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+ */
